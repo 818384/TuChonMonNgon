f2bf0d360354e0ee10305f314d3a607ea5fe67c4
18424081 <18424081@student.hcmus.edu.vn>
2020-10-05 03:47:16 +0700

    fix Mua button

--- js/custom.js
+++ js/custom.js
@@ -8,44 +8,6 @@ function closeNav() {
 	document.getElementById("websiteContent").style.marginLeft = "0";
 }
 
-$('.add-to-cart').on('click', function () {
-	var shoppingCart = $('.shopping-cart-plus-product');
-	var imgProduct = $(this).parent().parent().parent().find("img").eq(0);
-	if (imgProduct) {
-		var imgProductClone = imgProduct.clone()
-			.offset({
-				top: imgProduct.offset().top,
-				left: imgProduct.offset().left
-			})
-			.css({
-				'opacity': '0.5',
-				'position': 'absolute',
-				'height': 'auto',
-				'width': '255px',
-				'z-index': '2'
-			})
-			.appendTo($('body'))
-			.animate({
-				'top': shoppingCart.offset().top + 13,
-				'left': shoppingCart.offset().left + 15,
-				'width': 40,
-				'height': 'auto'
-			}, 1000, 'easeInOutExpo');
-
-		setTimeout(function () {
-			shoppingCart.animate({ zoom: "140%" }, 300);
-			shoppingCart.animate({ zoom: "100%" }, 300);
-		}, 1000);
-
-		imgProductClone.animate({
-			'width': 0,
-			'height': 0
-		}, function () {
-			$(this).detach()
-		});
-	}
-});
-
 function bindDataToHTML() {
 	let mainRow = $('#main-product-row');
 	let loadProducts = data.then(x => {
@@ -61,47 +23,91 @@ function bindDataToHTML() {
 				+ '	</div>'
 				+ '	<div class="card-description">'
 				+ '		<button type="button" class="btn btn-outline-success btn-round" onclick="openNav()">Xem món</button>'
-				+ '		<button type="button" class="btn btn-outline-danger btn-round add-to-cart" >Mua</i></button>'
+				+ '		<button type="button" class="btn btn-outline-danger btn-round add-to-cart">Mua</i></button>'
 				+ '	</div></div></div></div></div>');
 			console.log(x.products[i].name);
 		}
 	});
-}
-$('.add-to-cart-detail').on('click', function () {
-	var shoppingCart = $('.shopping-cart-plus-product');
-	var imgProduct = $(this).parent().parent().parent().parent().parent().parent().find("img").eq(0);
-	if (imgProduct) {
-		var imgProductClone = imgProduct.clone()
-		.offset({
-			top: imgProduct.offset().top,
-			left: imgProduct.offset().left
-		})
-		.css({
-			'opacity': '0.5',
-			'position': 'absolute',
-			'height': 'auto',
-			'width': '461px',
-			'z-index': '1051'
-		})
-		.appendTo($('body'))
-		.animate({
-			'top': shoppingCart.offset().top + 13,
-			'left': shoppingCart.offset().left + 15,
-			'width': 40,
-			'height': 'auto'
-		}, 1000, 'easeInOutExpo');
 
-		setTimeout(function () {
-			shoppingCart.animate({zoom: "140%"}, 300);
-			shoppingCart.animate({zoom: "100%"}, 300);
-		}, 1000);
-
-
-		imgProductClone.animate({
-			'width': 0,
-			'height': 0
-		}, function () {
-			$(this).detach()
+	$(document).ready(function() {
+		$('.add-to-cart').on('click', function () {
+			var shoppingCart = $('.shopping-cart-plus-product');
+			var imgProduct = $(this).parent().parent().parent().find("img").eq(0);
+			if (imgProduct) {
+				var imgProductClone = imgProduct.clone()
+					.offset({
+						top: imgProduct.offset().top,
+						left: imgProduct.offset().left
+					})
+					.css({
+						'opacity': '0.5',
+						'position': 'absolute',
+						'height': 'auto',
+						'width': '255px',
+						'z-index': '1031'
+					})
+					.appendTo($('body'))
+					.animate({
+						'top': shoppingCart.offset().top + 13,
+						'left': shoppingCart.offset().left + 15,
+						'width': 40,
+						'height': 'auto'
+					}, 1000, 'easeInOutExpo');
+		
+				setTimeout(function () {
+					shoppingCart.animate({ zoom: "140%" }, 300);
+					shoppingCart.animate({ zoom: "100%" }, 300);
+				}, 1000);
+		
+				imgProductClone.animate({
+					'width': 0,
+					'height': 0
+				}, function () {
+					$(this).detach()
+				});
+			}
 		});
-	}
-});
+	
+		$('.add-to-cart-detail').on('click', function () {
+			var shoppingCart = $('.shopping-cart-plus-product');
+			var imgProduct = $(this).parent().parent().parent().parent().parent().parent().find("img").eq(0);
+			if (imgProduct) {
+				var imgProductClone = imgProduct.clone()
+				.offset({
+					top: imgProduct.offset().top,
+					left: imgProduct.offset().left
+				})
+				.css({
+					'opacity': '0.5',
+					'position': 'absolute',
+					'height': 'auto',
+					'width': '461px',
+					'z-index': '1051'
+				})
+				.appendTo($('body'))
+				.animate({
+					'top': shoppingCart.offset().top + 13,
+					'left': shoppingCart.offset().left + 15,
+					'width': 40,
+					'height': 'auto'
+				}, 1000, 'easeInOutExpo');
+		
+				setTimeout(function () {
+					shoppingCart.animate({zoom: "140%"}, 300);
+					shoppingCart.animate({zoom: "100%"}, 300);
+				}, 1000);
+		
+		
+				imgProductClone.animate({
+					'width': 0,
+					'height': 0
+				}, function () {
+					$(this).detach()
+				});
+			}
+		});
+	});
+	
+	
+}
+
